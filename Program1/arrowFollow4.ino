3862caff98c650bff45793b1e105643dac456e7e
gihanchanaka <gihanchanaka@gmail.com>
2017-09-04 13:16:54 +0530

    First arrow finished

--- Program1/arrowFollow4.ino
+++ Program1/arrowFollow4.ino
@@ -41,7 +41,7 @@ void start(int boxColor) {
   //This uses the colour reading as 1-RED,2-GREEN.3=BLUE
 
 	Serial.println("I am at the start point of three arrows");
-	firstArrowFollow();
+	firstArrowFollow(boxColor);
 	trailAndErrorArrowFollow_Loop(boxColor);
 
 
@@ -77,7 +77,11 @@ void firstArrowFollow(int boxColor) {
   	}
   }
 
-
+  readSensorLine(reading);
+  while (sumOfArray(reading, 6) != 0) {
+    goFF();
+    readSensorLine(reading);
+  }
 
   readSensorLine(reading);
   while (sumOfArray(reading, 6) != 0) {
@@ -114,30 +118,23 @@ void firstArrowFollow(int boxColor) {
     for (int j = 0; j < 6; j++) {
       weightedSum += reading[j] * weight[j];
     }
-    //Serial.println("Weighted sum = ");
-    //Serial.println(weightedSum);
 
     if (weightedSum != 0) {
       if (weightedSum < 0) {
-//        Serial.println("Forward loop- Turn right");
         motorWrite(100, -100);
         delay(100);
       }
       else {
-//        Serial.println("Forward loop- Turn left");
         motorWrite(-100, 100);
         delay(100);
       }
     }
     else {
-      //This is the tricky part,
-      //Can this scenario ever come?
+      break;
 
     }
 
-  
-    motorWrite(100, 100);
-    delay(100);
+    goFF();
     readSensorLine(reading);
   }
 
