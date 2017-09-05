a77d62d4ff24b9717e47148e68fa6a3875b8cf6d
Harshana Weligampola <uwillfindharshana@gmail.com>
2017-09-05 11:45:24 +0530

    Update dest_check_harshana.ino

--- Program1/dest_check_harshana.ino
+++ Program1/dest_check_harshana.ino
@@ -1,56 +1,66 @@
 
 
-int binaryEncodedIRReading() {
+int binaryEncodedIRReading(){
   int ans = 0;
-  for (int i = 0; i < 6; i++) {
-    ans += reading[i] * (1 << (6 - i));
-  }
+  for (int i=0;i<6;i++){
+    ans += reading[i]*(1<<(6-i));
+  } 
   return ans;
 }
 
-bool isThisTheDestination(int boxColor) {
+bool isThisTheDestination(int boxColor){
   /*
-    Harshana
-    2017-9-4 7:30 P.M.
+  Harshana
+  2017-9-4 7:30 P.M.
 
 
-    Call me to see whether the robot has reached the destination
-    (call when all the IR sensors give positive values OR IN SOME SUSPICOUS OCCATION {DISCUSS THIS})
+  Call me to see whether the robot has reached the destination
+  (call when all the IR sensors give positive values OR IN SOME SUSPICOUS OCCATION {DISCUSS THIS})
 
-    return:- true if destination. false o.w.
-
-    if return false:
+  return:- true if destination. false o.w.
+  
+  if return false:
     robot will be at the position where the function was called
-    if return true:
+  if return true:
     robot will be at the position to drop the box
   */
 
   if (boxColor != getColorReading())
     return false;
 
-  int andsum = (1 << 7) - 1;
+  int andsum = (1<<7) - 1;
   int orsum = 0;
-  for (int i = 0; i < 3; i++) {
+  short int res = 5; //something divisible by 80
+  for (int i=0; i<res;i++){
     readSensorLine(reading);
     andsum = andsum && binaryEncodedIRReading;
     orsum = orsum || binaryEncodedIRReading;
+    goFoward(80/res);
   }
-  if (andsum == (1 << 7) - 1) {
+  if (andsum == (1<<7) - 1){
     // perfectly matched all the points.
     // good place to unload the box
-
+    goFoward(-80);
+    goFoward(-20);
     // TODO: goback needed distance
-    // return true;
-  } else if (((andsum >> 2) % 2 == 1 or (andsum >> 3) % 2 == 1) and (orsum % 2 == 1 or (orsum >> 5) % 2 == 1)) {
+    return true;
+  }else if (((andsum>>2)%2 == 1 or (andsum>>3)%2 == 1) and (orsum%2 == 1 or (orsum>>5)%2 == 1)){
     // now most probably destination is at a rotated position w.r.t robot
     // unlikely an arrow!!
-    //THINGS TO DISCUSS:
+    // THINGS TO DISCUSS:
     //  can 2 arrows give this
 
     // come back the half the distance that went
     // rotate a few steps
     // TO DISCUSS: HOW MUCH AND WHICH DIRECTION
-    //comback the other half
+    goFoward(-40);
+    if (sign_ws){
+      // first encounter is in right side
+      turnCW(10);
+    }else{
+      turnCW(-10);
+    }
+    // come back the other half
     // + a few more steps
 
     // now return false :(
@@ -60,5 +70,11 @@ bool isThisTheDestination(int boxColor) {
   }
 
   //come back the distance that went forward for checking
+  return false;
+}
+
+
+bool isThisTheDestinationRED(){
+  
   return false;
 }
